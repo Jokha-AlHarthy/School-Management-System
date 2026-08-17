33e761cf34860753ce9683052d9128cdb7f82b23
Jokha Al-Harthy <jokha.hamed.official@gmail.com>
2026-08-17 20:33:32 +0400

    feat(entities): Student extends Person + fields   [Task 1.2]

---
+++ src/entities/Student.java
@@ -0,0 +1,26 @@
+package entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Student extends Person{
+    //Attributes
+    private String gradeLevel;
+    private String enrollmentDate;
+    private List<String> enrolledSubjects;
+    private List<String> pastCourseRecord;
+    private String studentId;
+    private double feeBalance;
+    private boolean isScholarship;
+
+    public Student(String personId, String firstName, String lastName, String dateOfBirth, String gender, int phoneNumber, String email, String address, int nationalId, int age, String activeStatus) {
+        super(personId, firstName, lastName, dateOfBirth, gender, phoneNumber, email, address, nationalId, age, activeStatus);
+        this.gradeLevel = gradeLevel;
+        this.enrollmentDate = enrollmentDate;
+        this.enrolledSubjects = new ArrayList<>();
+        this.pastCourseRecord = new ArrayList<>();
+        this.studentId = studentId;
+        this.feeBalance = feeBalance;
+        this.isScholarship = isScholarship;
+    }
+}
