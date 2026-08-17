78e1929a29fe3dff959786d4004550c25270db32
Jokha Al-Harthy <jokha.hamed.official@gmail.com>
2026-08-17 20:56:29 +0400

    feat(entities): Student subject methods [Task 1.2]

--- src/entities/Student.java
+++ src/entities/Student.java
@@ -1,24 +1,22 @@
 package entities;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Student extends Person{
     //Attributes
     private String gradeLevel;
     private String enrollmentDate;
-    private List<String> enrolledSubjects;
-    private List<String> pastCourseRecord;
+    private String [] enrolledSubjects;
+    private String [] pastCourseRecord;
     private String studentId;
     private double feeBalance;
     private boolean isScholarship;
+    private int subjectCount= 0;
 
     public Student(String personId, String firstName, String lastName, String dateOfBirth, String gender, int phoneNumber, String email, String address, int nationalId, int age, String activeStatus) {
         super(personId, firstName, lastName, dateOfBirth, gender, phoneNumber, email, address, nationalId, age, activeStatus);
         this.gradeLevel = gradeLevel;
         this.enrollmentDate = enrollmentDate;
-        this.enrolledSubjects = new ArrayList<>();
-        this.pastCourseRecord = new ArrayList<>();
+        this.enrolledSubjects = new String [10];
+        this.pastCourseRecord = new String [10];
         this.studentId = studentId;
         this.feeBalance = feeBalance;
         this.isScholarship = isScholarship;
@@ -36,6 +34,30 @@ public class Student extends Person{
                 ", feeBalance=" + feeBalance +
                 ", isScholarship=" + isScholarship +
                 '}');
+    }
+
+    //addSubject(...) and hasSubject(...) — add one, check if present
+    public void addSubject(String subject) {
+        if(subjectCount < enrolledSubjects.length){
+            enrolledSubjects[subjectCount] = subject;
+            subjectCount++;
+        }
+    }
+
+    public boolean hasSubject(String subject) {
+        for (int i = 0; i < subjectCount; i++) {
+            if (enrolledSubjects[i].equalsIgnoreCase(subject)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //listSubjects() — prints all subjects
+    public void listSubjects() {
+        for (int i = 0; i < subjectCount; i++) {
+            System.out.println("- " + enrolledSubjects[i]);
+        }
     }
 
 }
