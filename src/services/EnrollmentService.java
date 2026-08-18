6acbad8b5ac38d5e654e47ef9cc45a1d8c29f04c
Jokha-AlHarthy <jokha.hamed.official@gmail.com>
2026-08-18 15:13:01 +0400

    feat(services): enroll overloads (x3) [Task 2.2]

---
+++ src/services/EnrollmentService.java
@@ -0,0 +1,38 @@
+package services;
+
+import entities.CourseRecord;
+import entities.Enrollment;
+import entities.Student;
+
+public class EnrollmentService {
+    private Enrollment[] enorllments =  new Enrollment[50];
+    private int count = 0;
+
+
+    //ids + date
+    public void enroll(String studentId, String courseId, String enrollDate){
+        if(count < enorllments.length){
+            String id = "ENR"+(count + 1);
+            enorllments[count] =  new Enrollment(id, studentId, courseId, enrollDate, "Active", "Standard", false);
+            count++;
+        }
+    }
+
+    // ids + date + term
+    public void enroll(String studentId, String courseId, String enrollDate, String term){
+        if(count < enorllments.length){
+            String id = "ENR"+(count + 1);
+            enorllments[count] =  new Enrollment(id, studentId, courseId, enrollDate, "Active", "Term: "+term, false);
+            count++;
+        }
+    }
+
+    //full objects + reason
+    public void enroll(Student student, CourseRecord course, String reason){
+        if(student != null && course != null && count < enorllments.length){
+            String id = "ENR"+(count + 1);
+            enorllments[count] =  new Enrollment(id, student.getStudentId(), course.getRecordId(), "08-Aug-26", "Active", reason, false);
+            count++;
+        }
+    }
+}
