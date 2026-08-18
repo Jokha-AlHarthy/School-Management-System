2f99a484fea164a50827b388f2d8885d371bd70a
Jokha-AlHarthy <jokha.hamed.official@gmail.com>
2026-08-18 08:53:55 +0400

    feat(entities): SeniorStudent extends Student + fields [Task 2.1]

---
+++ src/entities/SeniorStudent.java
@@ -0,0 +1,14 @@
+package entities;
+
+public class SeniorStudent extends Student{
+    //Attributes
+    private String major;
+    private double gpa;
+    private String graduationDate;
+    private int creditsEarned;
+
+    //constructor calling super(...)
+    public SeniorStudent(String personId, String firstName, String lastName, String dateOfBirth, String gender, int phoneNumber, String email, String address, int nationalId, int age, String activeStatus) {
+        super(personId, firstName, lastName, dateOfBirth, gender, phoneNumber, email, address, nationalId, age, activeStatus);
+    }
+}
