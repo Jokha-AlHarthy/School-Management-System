2177a3ee4632c004fcd54304d8f2b6924ab19280
Jokha-AlHarthy <jokha.hamed.official@gmail.com>
2026-08-19 08:24:46 +0400

    feat(interfaces): Updating Manageable interface

--- src/interfaces/Manageable.java
+++ src/interfaces/Manageable.java
@@ -2,7 +2,7 @@ package interfaces;
 
 public interface Manageable {
     //add an entity, remove by id, get all.
-    void add(Object item);
+    boolean add(Object item);
     boolean removeById(String id);
     Object[]getAll();
 }
